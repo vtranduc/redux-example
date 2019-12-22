b781ea2b6d2e12f542953c77f0292dbb76121850
Viet Nhien Tran Duc <vtranduc@spinvfx.com>
2019-12-22 18:42:34 -0500

    completed the example

--- src/index.js
+++ src/index.js
@@ -3,8 +3,58 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import allReducer from './reducers'
+import { Provider } from 'react-redux';
+
+const store = createStore(allReducer);
+
+// -------------------------------------------------------------
+
+// import { createStore } from 'redux';
+
+// //STORE -> GLOBALIZED STATE
+
+// // ACTION INCREMENT
+// const increment = () => {
+//   return {
+//     type: 'INCREMENT'
+//   }
+// }
+// const decrement = () => {
+//   return {
+//     type: 'DECREMENT'
+//   }
+// }
+
+// // REDUCER
+// const counter = (state = 0, action) => {
+//   switch(action.type) {
+//     case 'INCREMENT': 
+//       return state + 1;
+//     case 'DECREMENT':
+//       return state - 1;
+//   }
+// }
+
+// let store = createStore(counter);
+
+// //Display it in the console
+// store.subscribe(() => console.log(store.getState()))
+
+// // DISPATCH
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(decrement());
+
+// ---------------------------------------------------------------------------------
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
